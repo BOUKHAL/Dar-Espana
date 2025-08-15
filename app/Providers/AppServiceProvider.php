0c8b51a75db2691635e3df681217fb0c8c30cdd6
Hassan <hassanboukhal6@gmail.com>
2025-08-15 15:29:06 +0100

    authentification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // ➜ ajout de l'import
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // ➜ réglage pour éviter l'erreur "Specified key was too long"
+        Schema::defaultStringLength(191);
     }
 }
